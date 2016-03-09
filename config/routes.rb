082180b0d2a1223682baabc20ebbc179f9973c47
Spencer Griffin <spencekarl@gmail.com>
2016-03-09 12:53:01 -0500

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,8 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   Rails.application.routes.draw do
-    resources :contacts, only: [:new, :create]
-    root to: 'visitors#new'
-  end
+  resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
+  root to: 'visitors#new'
+end
 end
