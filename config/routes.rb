9f5f53358efac8dfd1ccdf0556447130f33b9f6b
Spencer Griffin <spencekarl@gmail.com>
2016-03-09 12:26:07 -0500

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   Rails.application.routes.draw do
+    resources :contacts, only: [:new, :create]
     root to: 'visitors#new'
   end
 end
